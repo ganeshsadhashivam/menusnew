a5bc8042cbe16413d2b84c6a15b91ce476807933
ganesh <ganeshsadhashivam@outlook.com>
2023-03-31 23:15:53 +0530

    menus finished

---
+++ src/MenuItem.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const MenuItem = ({ id, img, title, price, desc }) => {
+  return (
+    <article>
+      <img src={img} alt={title} />
+      <div className="item-info">
+        <header>
+          <h5>{title}</h5>
+          <span className="item-price">${price}</span>
+        </header>
+        <p className="item-text">{desc}</p>
+      </div>
+    </article>
+  );
+};
+
+export default MenuItem;
